21daa87dfad171d2a9c68b523e6f4eb773523394
sinastiller <sinastiller@gmx.de>
2023-01-12 19:34:28 +0000

    Adding the create_board()

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-'''
+"""
 Importing the randint function from the random module
-'''
+"""
 # import random
 
 # Declaring of Constant Variables
@@ -15,4 +15,13 @@ COMP_BOARD_GUESS = [["-"] * 8 for i in range(8)]
 # Converting letter to numbers for the grid
 GRID = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
 
-print(GRID)
+# Printing the board function
+
+
+def create_board(board):
+    print(" ", " ".join("12345678"))
+    for letter, row in zip("ABCDEFGH", board):
+        print(letter, " ".join(row))
+
+
+create_board(COMP_BOARD)
